c1550611f6ce033b7b1098b4e5d4047aba4e072d
Bferian <124202804+Bferian@users.noreply.github.com>
2026-05-04 22:56:00 -0300

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -1,8 +1,6 @@
 const { app, BrowserWindow, shell } = require("electron");
 const path = require("path");
 
-const isDev = process.env.NODE_ENV === "development";
-
 function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
@@ -19,11 +17,8 @@ function createWindow() {
     },
   });
 
-  if (isDev) {
-    win.loadURL("http://localhost:5173");
-  } else {
-    win.loadFile(path.join(__dirname, "../dist/index.html"));
-  }
+  // Carrega o HTML diretamente
+  win.loadFile(path.join(__dirname, "../nexus-bi-light.html"));
 
   win.once("ready-to-show", () => win.show());
 
